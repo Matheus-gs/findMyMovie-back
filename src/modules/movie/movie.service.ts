4bdec48eac612b7cc9a7b9628dc423607d5741f1
matheus <mgomes494@gmail.com>
2023-02-25 13:18:07 -0300

    refact: adding a constants folder + adding imdbRating to response object

--- src/modules/movie/movie.service.ts
+++ src/modules/movie/movie.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { IResponseGetMovieByTitle } from './interfaces/IResponseGetMovieByTitle';
 import { HttpService } from '@nestjs/axios';
+import { OMDB_API_BASE_URL } from 'src/constants';
+import { ResponseGetMovieByTitleDTO } from './types/ResponseGetMovieByTitleDTO';
+import { IResponseGetMovieByTitle } from './interfaces/IResponseGetMovieByTitle';
 
 @Injectable()
 export class MovieService {
   constructor(private readonly httpService: HttpService) {}
 
-  async getMovieByTitle(title: string): Promise<IResponseGetMovieByTitle> {
+  async getMovieByTitle(title: string): Promise<ResponseGetMovieByTitleDTO> {
     if (!title) return;
-    const url = `http://www.omdbapi.com/?apikey=9219e4d&t=${title}`;
+    const url = `${OMDB_API_BASE_URL}&t=${title}`;
 
-    const { data } =
-      await this.httpService.axiosRef.get<IResponseGetMovieByTitle>(url);
+    const { data } = await this.httpService.axiosRef.get<IResponseGetMovieByTitle>(url);
 
-    const { Title, Actors, Plot, Poster, Ratings } = data;
+    const { Title, Actors, Plot, Poster, Ratings, imdbRating } = data;
 
     return {
       Title,
@@ -21,6 +22,7 @@ export class MovieService {
       Plot,
       Poster,
       Ratings,
+      imdbRating,
     };
   }
 }
