31eb7b988aee3b42e171c20136f19e3fda3975ac
matheus <mgomes494@gmail.com>
2023-02-26 12:53:16 -0300

    docs: adding log with base url info when app starts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { startSwagger } from './docs/swagger';
 import { enableCors } from './config/cors';
-
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   enableCors(app);
   startSwagger(app);
   await app.listen(3000);
+  Logger.log("Server started on http://localhost:3000/docs")
 }
 bootstrap();
